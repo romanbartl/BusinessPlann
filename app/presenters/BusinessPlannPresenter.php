d9f178a3df216ddac0f21d7411c07cb71c00dd23
Roman Bártl <bbartl.roman@gmail.com>
2015-09-26 21:56:48 +0200

    BP adding template and presenter

---
+++ app/presenters/BusinessPlannPresenter.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Presenters;
+
+use Nette,
+    Nette\Application\UI\Presenter,
+    Nette\Application\UI\Form,
+    App\Model\UserManager;
+
+/**
+ * BusinessPlann Presenter
+ */
+
+class BusinessPlannPresenter extends BasePresenter
+{
+
+}
