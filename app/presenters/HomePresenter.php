d030e76df668479f7b36f6d98c2806e2b9933a46
Roman Bártl <bbartl.roman@gmail.com>
2015-11-02 23:59:17 +0100

    HOME - renaming BusinessPlann to Home

---
+++ app/presenters/HomePresenter.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Presenters;
+
+use Nette,
+    Nette\Application\UI\Presenter,
+    Nette\Application\UI\Form,
+    App\Model\UserManager;
+
+/**
+ * Home Presenter
+ */
+
+class HomePresenter extends BasePresenter
+{
+
+}
