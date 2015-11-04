cb396d4b685175309aaf06963f1202d2b5469346
Roman Bártl <bbartl.roman@gmail.com>
2015-11-04 15:52:27 +0100

    Some changes

--- app/presenters/RegistrationPresenter.php
+++ app/presenters/RegistrationPresenter.php
@@ -21,8 +21,7 @@ class RegistrationPresenter extends BasePresenter
      * Contruct
      * @param UserManager $userManager
      */
-    public function __construct(UserManager $userManager)
-    {
+    public function __construct(UserManager $userManager) {
         parent::__construct();
         $this->userManager = $userManager;
     }
@@ -33,40 +32,41 @@ class RegistrationPresenter extends BasePresenter
      *
      * TODO regulární výrazy
      */
-	protected function createComponentRegForm()
-    {
+	protected function createComponentRegForm() {
         $form = new Form;
 
         $form->addText('name')
                  ->setAttribute('class', 'input')
                  ->setAttribute('placeholder', 'Jméno')
-                 ->addRule(Form::FILLED, 'Zadajte své jméno')
+                 ->addRule(Form::FILLED, 'Vyplňte své jméno.')
                  ->addRule(Form::PATTERN, 'Jméno není ve správném tvaru!', 
                     '^[a-zěščřžýáíéťňďó]{1,}$');
 
         $form->addText('surname')
                  ->setAttribute('class', 'input')
                  ->setAttribute('placeholder', 'Příjmení')
-                 ->addRule(Form::FILLED, 'Pole nesmí zůstat prázdné')
+                 ->addRule(Form::FILLED, 'Vyplňte své příjmení.')
                  ->addRule(Form::PATTERN, 'Příjmení není ve správném tvaru!', 
                     '^[a-zěščřžýáíéťňďó]{1,}$');
 
         $form->addText('email')
                  ->setAttribute('class', 'input')
                  ->setAttribute('placeholder', 'E-mail')
-                 ->addRule(Form::FILLED, 'Pole nesmí zůstat prázdné')
+                 ->addRule(Form::FILLED, 'Vyplňte svůj email.')
+                 // TODO ->addRule($this->userManager->userExists($form['email']), 'Uživatel s tímto emailem již existuje.')
                  ->addRule(Form::PATTERN, 'Email není ve správném tvaru!', 
                     '^[A-Za-z0-9._-]+@[A-Za-z0-9]+\.[a-z]{1,4}$');
 
+        /* TODO podmínka pro vstup hesla (reg. výrazy) */
         $form->addPassword('passwd')
                  ->setAttribute('class', 'input')
                  ->setAttribute('placeholder', 'Heslo')             
-                 ->addRule(Form::FILLED, 'Zadajte své příjmení');
+                 ->addRule(Form::FILLED, 'Vyplňte své heslo.');
 
         $form->addPassword('passwdControl')
                  ->setAttribute('class', 'input')
                  ->setAttribute('placeholder', 'Heslo znovu')
-                 ->addRule(Form::FILLED, 'Zadajte své příjmení')
+                 ->addRule(Form::FILLED, 'Vyplňte kontrolu hesla.')
                  ->addRule(Form::EQUAL, 'Hesla se musí shodovat!', $form['passwd']);
 
         $form->addSubmit('regBut', 'Vytvořit bezplatný účet')
@@ -82,14 +82,10 @@ class RegistrationPresenter extends BasePresenter
      * @param Form $form
      * @param Array $values - sent values from form
      */
-    public function regFormSucceeded($form, $values)
-    {
-        /* TODO - will return faiuler message (email exists etc.)*/
-
+    public function regFormSucceeded($form, $values) {
         $this->userManager->register($values['name'], 
                                      $values['surname'],
                                      $values['email'],
-                                     $values['passwd'],
-                                     $values['passwdControl']);
+                                     $values['passwd']);
     }
 }
--- app/presenters/SigninPresenter.php
+++ app/presenters/SigninPresenter.php
@@ -13,13 +13,15 @@ use Nette,
 
 class SigninPresenter extends BasePresenter
 {
-	protected function createComponentSignInForm()
-	{
+	protected function createComponentSignInForm() {
 		$form = New Form();
 
 		$form->addText('email')
-			 ->setAttribute('class', 'input')
-			 ->setAttribute('placeholder', 'E-mail');
+			 	->setAttribute('class', 'input')
+			 	->setAttribute('placeholder', 'E-mail')
+			 	->addRule(Form::FILLED, 'Vyplňte svůj email.')
+                ->addRule(Form::PATTERN, 'Email není ve správném tvaru!', 
+                    '^[A-Za-z0-9._-]+@[A-Za-z0-9]+\.[a-z]{1,4}$');;
 
 		$form->addText('passwd')
 		     ->setAttribute('class', 'input')
@@ -27,7 +29,15 @@ class SigninPresenter extends BasePresenter
 
 		$form->addSubmit('signInBut', 'Přihlásit')
 			 ->setAttribute('class', 'submit_button');
-			 
+
+		$form->addCheckBox('stay_logged', 'Neodhlašovat');
+                  
+		$form->onSuccess[] = array($this, 'signInFormSucceeded');
+
 		return $form;
+	}
+
+	public function signInFormSucceeded($form, $values) {
+		
 	}
 }
