87c77d714191266a10f698f24839bbe3e9cfd60c
Roman Bártl <bbartl.roman@gmail.com>
2015-11-07 22:24:29 +0100

    BP model add database

--- app/model/BusinessplannModel.php
+++ app/model/BusinessplannModel.php
@@ -2,9 +2,14 @@
 
 namespace App\Model;
 
-use Nette;
+use Nette,
+	Nette\Security\User;
 
 class BusinessplannModel extends Nette\Object
 {
-	
+	private $database;
+
+	public function __construct(Nette\Database\Context $database) {
+		$this->database = $database;
+	}
 }
