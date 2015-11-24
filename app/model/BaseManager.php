ec75521d9cadffe3b6ce93b631bc8b5c3a396b1e
Roman Bártl <bbartl.roman@gmail.com>
2015-11-24 13:21:37 +0100

    TEMP - Change table rows names

--- app/model/BaseManager.php
+++ app/model/BaseManager.php
@@ -13,14 +13,15 @@ abstract class BaseManager extends Nette\Object
 {
 	const
 		USER_TABLE_NAME = 'user',
-		USER_COLUMN_ID = 'id_user',
+		USER_COLUMN_ID = 'id',
 		USER_COLUMN_NAME = 'name',
 		USER_COLUMN_SURNAME = 'surname',
 		USER_COLUMN_EMAIL = 'email',
 		USER_COLUMN_PASSWORD = 'password',
 		USER_COLUMN_PROFILE_PHOTO = 'profile_photo',
-		USER_COLUMN_BG_COLOR = 'bg_color_id_bg_color',
+		USER_COLUMN_BG_COLOR = 'bg_color_id',
 		USER_COLUMN_ROLE = 'role_id',
+		USER_COLUMN_LANGUAGE = 'language_id',
 
 		ROLE_COLUMN_ROLE = 'role';	
 }
